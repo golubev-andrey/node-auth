358b180e28005e1e05908edf5d764fa3669fa939
Andrei <andrey.diving@gmail.com>
2018-10-07 17:37:45 +0500

    finished auth flow

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,5 @@
-module.exports = {
-  googleClientID:
-    "1035402509941-7d2i4vfpk6apkdgr210agi60unemhpil.apps.googleusercontent.com",
-  googleClientSecret: "NFO-23nrlf480UZoJ5qSHhCT",
-  mongoURI: "mongodb://andrey_2:password2@ds223063.mlab.com:23063/email_dev",
-  cookieKey:
-    "asjjfiijqe0f9-2919390120jfljfljf0gheqwaz6456ulhgfb33334qwljhj930djasjk09pp"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./prod");
+} else {
+  module.exports = require("./dev");
+}
