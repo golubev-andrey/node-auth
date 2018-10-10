535f7b532c155df50de4e82bcf8e76ca437ccd3d
Andrei <andrey.diving@gmail.com>
2018-10-10 16:18:05 +0500

    react app

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>hi</h1>
+        <a href="/auth/google">sign in with google</a>
+      </div>
+    );
+  }
+}
+
+export default App;
